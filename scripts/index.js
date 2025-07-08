a9b589275d814177ffa1c52514efdfbe27ab00f9
Matthews <matthewsjordao@gmail.com>
2025-07-07 22:18:36 -0400

    reverted back to previous version

--- scripts/index.js
+++ scripts/index.js
@@ -140,15 +140,13 @@ editProfileBtn.addEventListener('click', () => {
   descriptionInput.value = profileDescriptionElement.textContent;
 
   // Reset errors and update button state before opening
-  resetValidation(profileFormElement, validationConfig); //
+  resetValidation(profileFormElement, validationConfig);
 
   openModal(editProfileModal);
 });
 
-// Open New Post Modal
+// Open New Post Modal (do NOT reset form here)
 newPostBtn.addEventListener('click', () => {
-  // Reset errors and disable the submit button before opening
-  resetValidation(addCardFormElement, validationConfig); // ADDED THIS LINE
   openModal(newPostModal);
 });
 
